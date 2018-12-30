579be17324153d658a8052bdba2f6da454a4a72b
guojs-github <guoojs@gmail.com>
2018-12-30 13:51:05 +0800

    20181230 gulp配置按照v4修改语法

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,21 @@ var gulp		=require('gulp'),  				//gulp基础库
     notify		=require('gulp-notify'),   		//提示
     less		=require('gulp-less');			//less编译
 
-//使用gulp.task('default')定义默认任务
-//在命令行使用gulp启动minify-css任务和auto任务
-gulp.task('default', ['minify-css', 'auto-less', 'auto-css'])
+gulp.task('clean', function() {
+	// 清除指定文件
+	return del([
+		'./dist/css/*.css',
+		'./dist/css/temp/*.css'
+	]);
+});
+
+//编译less
+//在命令行输入gulp less启动此任务
+gulp.task('less', function(){
+    return gulp.src('./src/**.less') 					// 找到less文件   
+		.pipe(less()) 							// 编译为css
+        .pipe(gulp.dest('dist/css/temp'))		// 另存为css
+})
 
 // 处理css
 gulp.task('minify-css', function(){
@@ -22,28 +34,14 @@ gulp.task('minify-css', function(){
        .pipe(gulp.dest('dist/css'))            			//输出 index.min.css
 });
 
-//在命令行gulp auto-less启动此任务
-gulp.task('auto-less',function(){    
-    gulp.watch('src/**.less', ['less']) //监听文件修改，当文件修改则执行less任务
+//在命令行gulp auto启动此任务
+gulp.task('auto', function(){    
+    gulp.watch('src/**.less', gulp.series('clean', 'less', 'minify-css')) //监听文件修改，当文件修改则执行less任务
 })
 
-//在命令行gulp auto-css启动此任务
-gulp.task('auto-css',function(){    
-    gulp.watch('dist/css/temp/**.css', ['minify-css']) //监听文件修改，当文件修改则执行minify-css任务
-})
+//使用gulp.task('default')定义默认任务
+//在命令行使用gulp启动minify-css任务和auto任务
+gulp.task('default', gulp.series('clean', 'less', 'minify-css', 'auto', function(done){
+	done();
+}))
 
-//编译less
-//在命令行输入gulp less启动此任务
-gulp.task('less', ['clean'], function(){
-    gulp.src('./src/**.less') 					// 找到less文件   
-		.pipe(less()) 							// 编译为css
-        .pipe(gulp.dest('dist/css/temp'))		// 另存为css
-})
-
-gulp.task('clean', function() {
-	// 清除指定文件
-	del([
-		'./dist/css/*.css',
-		'./dist/css/temp/*.css'
-	]);
-});
